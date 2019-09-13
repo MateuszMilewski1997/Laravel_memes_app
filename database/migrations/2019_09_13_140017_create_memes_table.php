b1ffd6c67d60178ce8b3577d0fdcd8fe5ce90b45
Mateusz <milewskimateusz28@gmail.com>
2019-09-13 19:26:12 +0200

    Create project and database

---
+++ database/migrations/2019_09_13_140017_create_memes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title',50);
+            $table->string('photoPath',100);
+            $table->bigInteger('likes');
+            $table->bigInteger('dislikes');
+            $table->bigInteger('author');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memes');
+    }
+}
---
+++ database/migrations/2019_09_13_140108_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->bigInteger('mem')->unsigned();
+            $table->string('content',200);
+            $table->bigInteger('author');
+            $table->foreign('mem')->references('id')->on('memes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
